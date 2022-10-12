9f458fefbc91d6597bbf1e92f58dfcde14651c04
ccgeo792 <ccgeo792@learner.DVUSD.org>
2022-10-12 13:57:07 -0700

    Update

---
+++ VB/MyGame/MyGame/Form1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.WallPictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Player = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox124 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox31 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox41 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.WallPictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.LabelF = New System.Windows.Forms.Label()
+        Me.Start = New System.Windows.Forms.Label()
+        Me.FINISH = New System.Windows.Forms.PictureBox()
+        CType(Me.WallPictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox124, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox31, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox41, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WallPictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FINISH, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 500
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 10000
+        '
+        'WallPictureBox3
+        '
+        Me.WallPictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox3.Location = New System.Drawing.Point(0, 0)
+        Me.WallPictureBox3.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox3.Name = "WallPictureBox3"
+        Me.WallPictureBox3.Size = New System.Drawing.Size(10, 292)
+        Me.WallPictureBox3.TabIndex = 6
+        Me.WallPictureBox3.TabStop = False
+        '
+        'WallPictureBox4
+        '
+        Me.WallPictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox4.Location = New System.Drawing.Point(0, 282)
+        Me.WallPictureBox4.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox4.Name = "WallPictureBox4"
+        Me.WallPictureBox4.Size = New System.Drawing.Size(437, 10)
+        Me.WallPictureBox4.TabIndex = 5
+        Me.WallPictureBox4.TabStop = False
+        '
+        'Player
+        '
+        Me.Player.Image = CType(resources.GetObject("Player.Image"), System.Drawing.Image)
+        Me.Player.Location = New System.Drawing.Point(26, 32)
+        Me.Player.Margin = New System.Windows.Forms.Padding(2)
+        Me.Player.Name = "Player"
+        Me.Player.Size = New System.Drawing.Size(59, 50)
+        Me.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Player.TabIndex = 0
+        Me.Player.TabStop = False
+        '
+        'WallPictureBox124
+        '
+        Me.WallPictureBox124.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox124.Location = New System.Drawing.Point(523, 0)
+        Me.WallPictureBox124.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox124.Name = "WallPictureBox124"
+        Me.WallPictureBox124.Size = New System.Drawing.Size(10, 292)
+        Me.WallPictureBox124.TabIndex = 7
+        Me.WallPictureBox124.TabStop = False
+        '
+        'WallPictureBox2
+        '
+        Me.WallPictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox2.Location = New System.Drawing.Point(96, 0)
+        Me.WallPictureBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox2.Name = "WallPictureBox2"
+        Me.WallPictureBox2.Size = New System.Drawing.Size(437, 10)
+        Me.WallPictureBox2.TabIndex = 8
+        Me.WallPictureBox2.TabStop = False
+        '
+        'WallPictureBox31
+        '
+        Me.WallPictureBox31.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox31.Location = New System.Drawing.Point(261, 209)
+        Me.WallPictureBox31.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox31.Name = "WallPictureBox31"
+        Me.WallPictureBox31.Size = New System.Drawing.Size(10, 83)
+        Me.WallPictureBox31.TabIndex = 9
+        Me.WallPictureBox31.TabStop = False
+        '
+        'WallPictureBox41
+        '
+        Me.WallPictureBox41.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox41.Location = New System.Drawing.Point(333, 0)
+        Me.WallPictureBox41.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox41.Name = "WallPictureBox41"
+        Me.WallPictureBox41.Size = New System.Drawing.Size(10, 141)
+        Me.WallPictureBox41.TabIndex = 10
+        Me.WallPictureBox41.TabStop = False
+        '
+        'WallPictureBox5
+        '
+        Me.WallPictureBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox5.Location = New System.Drawing.Point(96, 96)
+        Me.WallPictureBox5.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox5.Name = "WallPictureBox5"
+        Me.WallPictureBox5.Size = New System.Drawing.Size(10, 196)
+        Me.WallPictureBox5.TabIndex = 11
+        Me.WallPictureBox5.TabStop = False
+        '
+        'WallPictureBox6
+        '
+        Me.WallPictureBox6.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox6.Location = New System.Drawing.Point(190, 0)
+        Me.WallPictureBox6.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox6.Name = "WallPictureBox6"
+        Me.WallPictureBox6.Size = New System.Drawing.Size(10, 112)
+        Me.WallPictureBox6.TabIndex = 12
+        Me.WallPictureBox6.TabStop = False
+        '
+        'WallPictureBox7
+        '
+        Me.WallPictureBox7.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox7.Location = New System.Drawing.Point(427, 151)
+        Me.WallPictureBox7.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox7.Name = "WallPictureBox7"
+        Me.WallPictureBox7.Size = New System.Drawing.Size(10, 141)
+        Me.WallPictureBox7.TabIndex = 13
+        Me.WallPictureBox7.TabStop = False
+        '
+        'WallPictureBox8
+        '
+        Me.WallPictureBox8.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox8.Location = New System.Drawing.Point(96, 209)
+        Me.WallPictureBox8.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox8.Name = "WallPictureBox8"
+        Me.WallPictureBox8.Size = New System.Drawing.Size(86, 10)
+        Me.WallPictureBox8.TabIndex = 14
+        Me.WallPictureBox8.TabStop = False
+        '
+        'WallPictureBox9
+        '
+        Me.WallPictureBox9.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox9.Location = New System.Drawing.Point(190, 102)
+        Me.WallPictureBox9.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox9.Name = "WallPictureBox9"
+        Me.WallPictureBox9.Size = New System.Drawing.Size(64, 10)
+        Me.WallPictureBox9.TabIndex = 15
+        Me.WallPictureBox9.TabStop = False
+        '
+        'WallPictureBox10
+        '
+        Me.WallPictureBox10.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox10.Location = New System.Drawing.Point(261, 209)
+        Me.WallPictureBox10.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox10.Name = "WallPictureBox10"
+        Me.WallPictureBox10.Size = New System.Drawing.Size(64, 10)
+        Me.WallPictureBox10.TabIndex = 16
+        Me.WallPictureBox10.TabStop = False
+        '
+        'WallPictureBox11
+        '
+        Me.WallPictureBox11.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.WallPictureBox11.Location = New System.Drawing.Point(333, 72)
+        Me.WallPictureBox11.Margin = New System.Windows.Forms.Padding(2)
+        Me.WallPictureBox11.Name = "WallPictureBox11"
+        Me.WallPictureBox11.Size = New System.Drawing.Size(104, 10)
+        Me.WallPictureBox11.TabIndex = 17
+        Me.WallPictureBox11.TabStop = False
+        '
+        'LabelF
+        '
+        Me.LabelF.AutoSize = True
+        Me.LabelF.Location = New System.Drawing.Point(461, 278)
+        Me.LabelF.Name = "LabelF"
+        Me.LabelF.Size = New System.Drawing.Size(34, 13)
+        Me.LabelF.TabIndex = 18
+        Me.LabelF.Text = "Finish"
+        '
+        'Start
+        '
+        Me.Start.AutoSize = True
+        Me.Start.Location = New System.Drawing.Point(29, 8)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(29, 13)
+        Me.Start.TabIndex = 19
+        Me.Start.Text = "Start"
+        '
+        'FINISH
+        '
+        Me.FINISH.Location = New System.Drawing.Point(427, 294)
+        Me.FINISH.Name = "FINISH"
+        Me.FINISH.Size = New System.Drawing.Size(100, 23)
+        Me.FINISH.TabIndex = 20
+        Me.FINISH.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(533, 292)
+        Me.Controls.Add(Me.FINISH)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.LabelF)
+        Me.Controls.Add(Me.WallPictureBox11)
+        Me.Controls.Add(Me.WallPictureBox10)
+        Me.Controls.Add(Me.WallPictureBox9)
+        Me.Controls.Add(Me.WallPictureBox8)
+        Me.Controls.Add(Me.WallPictureBox7)
+        Me.Controls.Add(Me.WallPictureBox6)
+        Me.Controls.Add(Me.WallPictureBox5)
+        Me.Controls.Add(Me.WallPictureBox41)
+        Me.Controls.Add(Me.WallPictureBox31)
+        Me.Controls.Add(Me.WallPictureBox2)
+        Me.Controls.Add(Me.WallPictureBox124)
+        Me.Controls.Add(Me.WallPictureBox3)
+        Me.Controls.Add(Me.WallPictureBox4)
+        Me.Controls.Add(Me.Player)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Maze Game"
+        CType(Me.WallPictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox124, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox31, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox41, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WallPictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FINISH, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Player As PictureBox
+    Friend WithEvents WallPictureBox4 As PictureBox
+    Friend WithEvents WallPictureBox3 As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents WallPictureBox124 As PictureBox
+    Friend WithEvents WallPictureBox2 As PictureBox
+    Friend WithEvents WallPictureBox31 As PictureBox
+    Friend WithEvents WallPictureBox41 As PictureBox
+    Friend WithEvents WallPictureBox5 As PictureBox
+    Friend WithEvents WallPictureBox6 As PictureBox
+    Friend WithEvents WallPictureBox7 As PictureBox
+    Friend WithEvents WallPictureBox8 As PictureBox
+    Friend WithEvents WallPictureBox9 As PictureBox
+    Friend WithEvents WallPictureBox10 As PictureBox
+    Friend WithEvents WallPictureBox11 As PictureBox
+    Friend WithEvents LabelF As Label
+    Friend WithEvents Start As Label
+    Friend WithEvents FINISH As PictureBox
+End Class
---
+++ VB/MyGame/MyGame/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Const Speed As Integer = 5
+    Dim direction As Point
+
+    'This is where you can handle any collisions in the game
+    '
+    '  p1 has collided with p2
+    '  you can return true to let it go through it
+    '  or 
+    '  you can return false to not let it go through
+    '
+    '  you could also do other actions like change directions or add points etc.
+    Public Function Collision(p1 As PictureBox, p2 As PictureBox) As Boolean
+        Debug.Print(p1.Name + " ran into " + p2.Name)
+
+        If p2.Name.ToUpper.StartsWith("FINISH") Then
+            Me.BackColor = Color.Green
+        End If
+
+        'Anything that runs into a wall will stop
+        If p2.Name.ToUpper.StartsWith("WALL") Then
+            Return False 'don't let anything through walls
+        End If
+
+        'if none of the above happened, let the object move
+        Return True ' let it move
+    End Function
+
+    'Handle the keyboard - you could add your own keys if you wanted
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        Select Case e.KeyCode
+            Case Keys.W
+                direction = New Point(0, -Speed)
+                moveto(Player, 0, -Speed)
+            Case Keys.S
+                direction = New Point(0, Speed)
+                moveto(Player, 0, Speed)
+            Case Keys.A
+                direction = New Point(-Speed, 0)
+                moveto(Player, -Speed, 0)
+            Case Keys.D
+                direction = New Point(Speed, 0)
+                moveto(Player, Speed, 0)
+            Case Keys.Space
+        End Select
+    End Sub
+    'Don't mess with the code below, this updates the game
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        UpdateGame()
+    End Sub
+
+    Private Sub WallPictureBox3_Click(sender As Object, e As EventArgs) Handles WallPictureBox3.Click
+
+    End Sub
+End Class
